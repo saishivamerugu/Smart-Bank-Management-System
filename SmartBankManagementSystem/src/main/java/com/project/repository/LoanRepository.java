8d63ad719d77e60c9bcb5ae36d31c38d227db531
Sai Shiva <146641544+saishivamerugu@users.noreply.github.com>
2026-08-06 03:10:20 +0530

    Format LoanRepository.java for consistency

--- SmartBankManagementSystem/src/main/java/com/project/repository/LoanRepository.java
+++ SmartBankManagementSystem/src/main/java/com/project/repository/LoanRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.entity.Loan;
 
-public interface LoanRepository
-        extends JpaRepository<Loan, Integer> {
+public interface LoanRepository extends JpaRepository<Loan, Integer> {
 
     List<Loan> findByCustomerEmail(String email);
 
